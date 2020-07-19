166df96326b22ce10e078d505524f5a5c3797323
Mino777 <jomino7777@gmail.com>
2020-07-19 18:01:38 +0900

    <add> MagCollectionView in HomeViewController

---
+++ ios/Home/InHomeViewController/MagCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MagCollectionViewCell.swift
+//  VIBE
+//
+//  Created by 조민호 on 2020/07/19.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class MagCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var MagImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
