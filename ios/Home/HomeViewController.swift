188109e1e51bf48f13f1b0d885caea9ced11d52a
Mino777 <jomino7777@gmail.com>
2020-07-19 14:27:59 +0900

    <add> RecentlyCollectionView in HomeViewController

--- ios/Home/HomeViewController.swift
+++ ios/Home/HomeViewController.swift
@@ -14,8 +14,9 @@ class HomeViewController: BaseViewController {
 
     @IBOutlet weak var TitleCollectionView: UICollectionView!
     @IBOutlet weak var RecommendCollectionView: UICollectionView!
+    @IBOutlet weak var RecentlyCollectionView: UICollectionView!
     
-    let data = ["test1", "test2", "test3", "test4", "test5", "test6"]
+    let data = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"]
         
         var currentIndex: CGFloat = 0
         
@@ -28,12 +29,17 @@ class HomeViewController: BaseViewController {
         override func viewDidLoad() {
             super.viewDidLoad()
             
-            let nibName = UINib(nibName: "TitleCollectionViewCell", bundle: nil)
-            TitleCollectionView.register(nibName, forCellWithReuseIdentifier: "cell")
-            let secondNibName = UINib(nibName: "RecommendCollectionViewCell", bundle: nil)
-            RecommendCollectionView.register(secondNibName, forCellWithReuseIdentifier: "cell")
+            let TitlenibName = UINib(nibName: "TitleCollectionViewCell", bundle: nil)
+            TitleCollectionView.register(TitlenibName, forCellWithReuseIdentifier: "cell")
+            let RecommendNibName = UINib(nibName: "RecommendCollectionViewCell", bundle: nil)
+            RecommendCollectionView.register(RecommendNibName, forCellWithReuseIdentifier: "cell")
+            let RecentlyNibName = UINib(nibName: "RecentlyCollectionViewCell", bundle: nil)
+            RecentlyCollectionView.register(RecentlyNibName, forCellWithReuseIdentifier: "cell")
             
             TitleCollectionView.translatesAutoresizingMaskIntoConstraints = false
+            RecommendCollectionView.translatesAutoresizingMaskIntoConstraints = false
+            RecentlyCollectionView.translatesAutoresizingMaskIntoConstraints = false
+
             // width, height 설정
             let cellWidth = floor(view.frame.width * cellRatio)
             let cellHeight = floor(view.frame.height * cellRatio)
@@ -52,9 +58,14 @@ class HomeViewController: BaseViewController {
             TitleCollectionView.dataSource = self
             RecommendCollectionView.delegate = self
             RecommendCollectionView.dataSource = self
+            RecentlyCollectionView.delegate = self
+            RecentlyCollectionView.dataSource = self
             
             // 스크롤 시 빠르게 감속 되도록 설정
             TitleCollectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+            RecommendCollectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+            RecentlyCollectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+
         }
         
     }
@@ -68,6 +79,9 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
             if collectionView == RecommendCollectionView {
                 return 1
             }
+            if collectionView == RecentlyCollectionView {
+                return 1
+            }
             
             return 0
         }
@@ -77,6 +91,9 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
                 return data.count
             }
             if collectionView == RecommendCollectionView {
+                return data.count
+            }
+            if collectionView == RecentlyCollectionView {
                 return data.count
             }
             
@@ -106,48 +123,60 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
                 cell.alpha = 0.5
                 return cell
             }
+            
+            if collectionView == RecentlyCollectionView {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as!
+                    RecentlyCollectionViewCell
+                cell.RecentlyTitleLabel.text = data[indexPath.row]
+                cell.RecentlyImageView.image = UIImage(named: "recently_test2")
+                cell.backgroundColor = .white
+                cell.RecentlySubTitleLabel.text = data[indexPath.row]
+                cell.alpha = 0.5
+                cell.sizeToFit()
+                return cell
+            }
             return cell
         }
     }
 
-@available(iOS 13.0, *)
-extension HomeViewController : UIScrollViewDelegate {
-        
-        func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
-        {
-            // item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
-            let layout = self.TitleCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
-            let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
-            
-            // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
-            // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
-            var offset = targetContentOffset.pointee
-            let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
-            var roundedIndex = round(index)
-            
-            // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
-            // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
-            // 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
-            if scrollView.contentOffset.x > targetContentOffset.pointee.x {
-                roundedIndex = floor(index)
-            } else if scrollView.contentOffset.x < targetContentOffset.pointee.x {
-                roundedIndex = ceil(index)
-            } else {
-                roundedIndex = round(index)
-            }
-            
-            if isOneStepPaging {
-                if currentIndex > roundedIndex {
-                    currentIndex -= 1
-                    roundedIndex = currentIndex
-                } else if currentIndex < roundedIndex {
-                    currentIndex += 1
-                    roundedIndex = currentIndex
-                }
-            }
-            
-            // 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
-            offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
-            targetContentOffset.pointee = offset
-        }
-    }
+//@available(iOS 13.0, *)
+//extension HomeViewController : UIScrollViewDelegate {
+//
+//        func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+//        {
+//            // item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
+//            let layout = self.TitleCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//            let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+//
+//            // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
+//            // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
+//            var offset = targetContentOffset.pointee
+//            let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+//            var roundedIndex = round(index)
+//
+//            // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
+//            // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
+//            // 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
+//            if scrollView.contentOffset.x > targetContentOffset.pointee.x {
+//                roundedIndex = floor(index)
+//            } else if scrollView.contentOffset.x < targetContentOffset.pointee.x {
+//                roundedIndex = ceil(index)
+//            } else {
+//                roundedIndex = round(index)
+//            }
+//
+//            if isOneStepPaging {
+//                if currentIndex > roundedIndex {
+//                    currentIndex -= 1
+//                    roundedIndex = currentIndex
+//                } else if currentIndex < roundedIndex {
+//                    currentIndex += 1
+//                    roundedIndex = currentIndex
+//                }
+//            }
+//
+//            // 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
+//            offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+//            targetContentOffset.pointee = offset
+//        }
+//    }
