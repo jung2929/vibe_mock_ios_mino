a3e901415ca1c156f4155fc1083b1f0c23ef7965
Mino777 <jomino7777@gmail.com>
2020-07-20 16:17:34 +0900

    <add>  DJViewController Setting

---
+++ ios/DJ/DJRecentlyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DJRecentlyCollectionViewCell.swift
+//  VIBE
+//
+//  Created by 조민호 on 2020/07/20.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class DJRecentlyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- ios/DJ/DJViewController.swift
+++ ios/DJ/DJViewController.swift
@@ -10,8 +10,68 @@ import UIKit
 
 class DJViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet weak var DJRecentlyCollectionView: UICollectionView!
+    @IBOutlet weak var DJFeelCollectionView: UICollectionView!
+    @IBOutlet weak var DjGenreCollectionView: UICollectionView!
+    
+    
+    let data = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"]
+        let rankData = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+                
+            override func viewDidLoad() {
+                    super.viewDidLoad()
+                
+                    
+                let DJRecentlyNibName = UINib(nibName: "DJRecentlyCollectionViewCell", bundle: nil)
+                DJRecentlyCollectionView.register(DJRecentlyNibName, forCellWithReuseIdentifier: "cell")
+                    
+                    
+                DJRecentlyCollectionView.translatesAutoresizingMaskIntoConstraints = false
 
-    }
+
+                DJRecentlyCollectionView.delegate = self
+                DJRecentlyCollectionView.dataSource = self
+                    
+                    
+                // 스크롤 시 빠르게 감속 되도록 설정
+                DJRecentlyCollectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+        }
+                
 }
+
+    extension DJViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+                func numberOfSections(in collectionView: UICollectionView) -> Int {
+                    if collectionView == DJRecentlyCollectionView {
+                        return 1
+                    }
+                    
+                    return 0
+    }
+                
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                    if collectionView == DJRecentlyCollectionView {
+                        return data.count
+                    }
+                    
+                    return 0
+}
+                
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if collectionView == DJRecentlyCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! DJRecentlyCollectionViewCell
+//                        cell.TopTitleLabel.text = data[indexPath.row]
+//                        cell.TopImageView.image = UIImage(named: "recently_test2")
+//                        cell.backgroundColor = .white
+//                        cell.TopSubTitleLabel.text = data[indexPath.row]
+//                        cell.TopRankLabel.text = rankData[indexPath.row]
+//                        cell.alpha = 0.5
+//                        cell.sizeToFit()
+                return cell
+                }
+                return cell
+        }
+}
+
+
